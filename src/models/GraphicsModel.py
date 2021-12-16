80c91bb5949d6ddf4b9fa5a56a0223d2ce2bb126
quizzle <unknown>
2021-12-16 17:35:11 +0100

    line smoothing working

--- src/models/GraphicsModel.py
+++ src/models/GraphicsModel.py
@@ -25,9 +25,18 @@ class GraphicsModel():
         self.figures.clear()
 
     def addPoint(self, point):
-        #addPoint gets called by CVModel when a new tracking coordinate is available
-        self.figures[-1].addPoint(point)
-        self.bSpline()
+        """addPoint gets called by CVModel when a new tracking coordinate is available"""
+
+        #dont add identical points
+        points = self.figures[-1].points
+        if (len(points) == 0):
+            self.figures[-1].addPoint(point)
+        else:
+            lastPoint = points[-1]
+            if(lastPoint[0] == point[0] and lastPoint[1] == point[1]):
+                return
+            self.figures[-1].addPoint(point)
+            self.bSpline()  #smooth line
 
     def safeFigures(self):
         dialog = QFileDialog()
@@ -45,34 +54,26 @@ class GraphicsModel():
             file.close()
 
     def bSpline(self):
-        """smooth out the last 5 points of drawing"""
+        """smooth out the last points of stroke"""
         x = []
         y = []
-        #only last 5 points
-        points = self.figures[-1].points.copy()
-        if(len(points) < 5):
+        numberOfSmoothedPoints = 15
+        points = self.figures[-1].points
+        if(len(points) < numberOfSmoothedPoints):
             return
-        for i in range(len(points)-5-1,len(points)-1):
+        for i in range(len(points)-numberOfSmoothedPoints,len(points)):
             x.append(points[i][0])
-            y.append(points[i][1])
-
-
-        
+            y.append(points[i][1]) 
         tck, *rest = interpolate.splprep([x,y], k=3, s=300)#define k for order, define s for smoothness
-        u = np.linspace(0,1,num=20)    #20 interpolation values between two points
+        u = np.linspace(0,1,num=numberOfSmoothedPoints)
         smooth = interpolate.splev(u, tck)
-
         smoothList = []
         for i in range(0, len(smooth[0])):
             p = (smooth[0][i], smooth[1][i])
             smoothList.append(p)
-            #hier zu ppoints appenden
+    
+        self.figures[-1].points[-numberOfSmoothedPoints:] = smoothList
 
-
-
-        self.figures[-1].points[-5:] = smoothList
-        #print(smooth)
-        #print("")
 
 
 
