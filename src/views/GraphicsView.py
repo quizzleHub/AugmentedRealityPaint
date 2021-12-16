9f119addae90fc823c75b68d98ed83f7a964dd52
quizzle <unknown>
2021-12-16 10:33:28 +0100

    getter graphicsView

--- src/views/GraphicsView.py
+++ src/views/GraphicsView.py
@@ -19,10 +19,51 @@ class GraphicsView:
         self.currentBrushStyle = 1 #solid style
         self.currentPenCapStyle = 32 #rounded edges
 
-        #self.lineType = Qt.SolideLine
-        #self.pen.setStyle(3)
-        #self.pen.setBrush(QtGui.QColor)
+    def updateCanvas(self, image):
 
+        transformedImage = image.transformed(QTransform().scale(-1, 1)) #mirror
+
+        self.painter.begin(transformedImage)
+        self.pen.setWidthF(self.currentStrokeWidth)
+        self.pen.setBrush(QBrush(self.currentPenColor,self.currentBrushStyle))
+        self.pen.setCapStyle(self.currentPenCapStyle)
+        self.pen.setStyle(self.currentStrokePattern)
+        self.painter.setPen(self.pen)
+        self.painter.setRenderHint(QPainter.SmoothPixmapTransform, True)
+        self.painter.setRenderHint(QPainter.HighQualityAntialiasing, True)
+        
+
+        figures = self.grafikModel.getFigures()
+
+        for f in figures:
+            points = f.getPoints()
+            for i in range(1, len(points)):
+                p1 = points[i-1]
+                p2 = points[i]
+                self.painter.drawLine(p1[0], p1[1], p2[0], p2[1])
+
+        self.painter.end()
+        
+        scaledImage = transformedImage.scaled(self.canvas.width(), self.canvas.height(), Qt.KeepAspectRatio)
+        self.canvas.setPixmap(QPixmap.fromImage(scaledImage))
+
+
+    #____getter__________
+
+    def getStrokeColor(self):
+        return self.currentPenColor
+    
+    def getStrokeWidth(self):
+        return self.currentStrokeWidth
+
+    def getStrokePattern(self):
+        return self.currentStrokePattern
+
+    def getBrushStyle(self):
+        return self.currentBrushStyle
+
+    def getPenCapStyle(self):
+        return self.currentPenCapStyle
 
     #____setter__________
     def setCanvas(self, canvas):
@@ -42,34 +83,3 @@ class GraphicsView:
 
     def setPenCapStyle(self, penCapStyle):
         self.currentPenCapStyle = penCapStyle
-     
-
-
-    def updateCanvas(self, image):
-
-        transformedImage = image.transformed(QTransform().scale(-1, 1)) #mirror
-
-        self.painter.begin(transformedImage)
-        self.pen.setWidthF(self.currentStrokeWidth)
-        self.pen.setBrush(QBrush(self.currentPenColor,self.currentBrushStyle))
-        self.pen.setCapStyle(self.currentPenCapStyle)
-        self.pen.setStyle(self.currentStrokePattern)
-        self.painter.setPen(self.pen)
-
-        figures = self.grafikModel.getFigures()
-
-        for f in figures:
-            points = f.getPoints()
-            for i in range(1, len(points)):
-                p1 = points[i-1]
-                p2 = points[i]
-                self.painter.drawLine(p1[0], p1[1], p2[0], p2[1])
-
-        self.painter.end()
-        
-        scaledImage = transformedImage.scaled(self.canvas.width(), self.canvas.height(), Qt.KeepAspectRatio)
-        self.canvas.setPixmap(QPixmap.fromImage(scaledImage))
-
-
-
-
