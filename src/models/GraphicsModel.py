3468bbe8bf66c8b9cf57a6c21cd4c21c3885060e
quizzle <unknown>
2021-12-17 17:25:21 +0100

    fix performance issue with line smoothing

--- src/models/GraphicsModel.py
+++ src/models/GraphicsModel.py
@@ -28,6 +28,7 @@ class GraphicsModel():
                     return
                 self.figures[-1].addPoint(point)
                 self.bSpline()  #smooth line
+
         elif self.mode == 1:                        #if erasingMode find and delete figure
             figureIndex = self.findFigure(point)
             if figureIndex != None:
@@ -72,22 +73,23 @@ class GraphicsModel():
         """smooth out the last points of stroke"""
         x = []
         y = []
-        numberOfSmoothedPoints = 15
+        numberOfSmoothedPoints = 7
         points = self.figures[-1].points
         if(len(points) < numberOfSmoothedPoints):
             return
         for i in range(len(points)-numberOfSmoothedPoints,len(points)):
             x.append(points[i][0])
             y.append(points[i][1]) 
-        tck, *rest = interpolate.splprep([x,y], k=3, s=300)#define k for order, define s for smoothness
+        tck, *rest = interpolate.splprep([x,y], k=3) #k = order, s = smoothness
         u = np.linspace(0,1,num=numberOfSmoothedPoints)
         smooth = interpolate.splev(u, tck)
         smoothList = []
         for i in range(0, len(smooth[0])):
             p = (smooth[0][i], smooth[1][i])
             smoothList.append(p)
-    
-        self.figures[-1].points[-numberOfSmoothedPoints:] = smoothList
+
+        del self.figures[-1].points[-numberOfSmoothedPoints:]
+        self.figures[-1].points += smoothList
 
     def findFigure(self, point):
         #Algorithm to find a figure
