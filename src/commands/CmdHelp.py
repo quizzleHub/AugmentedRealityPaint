2fab1770bd44594a0b47bc60290b6baeacd3bb5f
quizzle <unknown>
2021-12-17 23:33:54 +0100

    added help html dialog

--- src/commands/CmdHelp.py
+++ src/commands/CmdHelp.py
@@ -1,5 +1,7 @@
 from commands.CommandInterface import CommandInterface
-from PyQt5.QtWidgets import QMessageBox
+from PyQt5 import QtCore, QtWidgets
+import codecs
+from pathlib import Path
 
 #https://github.com/smoqadam/pyqt-md-reader/blob/master/main.py
 
@@ -9,18 +11,44 @@ class CmdHelp(CommandInterface):
         self.view = view                
         self.model = model
         self.isUndoableBool = False
+        cwd = Path.cwd()
+        self.htmlContent = codecs.open(cwd/"commands"/"Help.html", 'r').read()
     def execute(self, *args):
-        msg = QMessageBox()
-        msg.setIcon(QMessageBox.Information)
-        msg.setText("This is a message box")
-        msg.setInformativeText("This is additional information")
-        msg.setWindowTitle("MessageBox demo")
-        msg.setDetailedText("The details are as follows:")
-        msg.setStandardButtons(QMessageBox.Ok)
-        msg.exec_()     
+        self.helpDialog = QtWidgets.QDialog()
+        self.helpDialogUi = Ui_Help()
+        self.helpDialogUi.setupUi(self.helpDialog)
+        self.helpDialogUi.buttonBox.clicked.connect(self.quitHelp)
+        self.helpDialogUi.textEdit.textCursor().insertHtml(self.htmlContent)
+        self.helpDialog.exec()
+
     def redo(self):
         pass
     def undo(self):
         pass
     def isUndoable(self):
         return self.isUndoableBool
+    def quitHelp(self):
+        self.helpDialog.close()
+
+
+
+
+class Ui_Help(object):
+    def setupUi(self, Help):
+        Help.setObjectName("Help")
+        Help.resize(768, 548)
+        Help.setWindowTitle("Help")
+        self.verticalLayout = QtWidgets.QVBoxLayout(Help)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.textEdit = QtWidgets.QTextEdit(Help)
+        self.textEdit.setObjectName("textEdit")
+        self.textEdit.setReadOnly(True)
+        self.verticalLayout.addWidget(self.textEdit)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Help)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setCenterButtons(True)
+        self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
+        
+
